d37a5fb67397fb9bb0d2306dfb9d6678f4c32ed2
Alexander St Laurent <stlaurent.alexander@protonmail.com>
2019-03-30 16:15:29 -0400

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "extends": ["eslint:recommended"],
+    "extends": ["eslint:recommended", "plugin:prettier/recommended"],
     "env": {
         "es6": true,
         "browser": true,
@@ -14,22 +14,13 @@ module.exports = {
         "Mousetrap": true
     },
     "rules": {
-        "array-bracket-newline": ["error", "consistent"],
         "array-element-newline": ["off",
             {
                 "multiline": true,
                 "minItems": 4
             }],
-        "brace-style": ["error", "stroustrup"],
         "func-names": ["error", "never"],
         "func-style": ["error", "declaration"],
-        "function-paren-newline": ["error", "consistent"],
-        "indent": ["error",
-            4,
-            {
-                "CallExpression": {"arguments": "first"},
-                "SwitchCase": 1
-            }],
         "max-len": ["error",
             {
                 "code": 100,
@@ -41,12 +32,6 @@ module.exports = {
         "max-lines-per-function": ["error", 100],
         "max-params": ["error", 5],
         "max-statements": ["error", {"max": 50}],
-        "multiline-ternary": ["error", "always-multiline"],
-        "no-extra-parens": ["error",
-            "all",
-            {
-                "nestedBinaryExpressions": false
-            }],
         "no-magic-numbers": ["off", // TODO: make stricter
             {
                 "detectObjects": true,
@@ -57,20 +42,8 @@ module.exports = {
                 "allowForLoopAfterthoughts": true
             }],
         "no-use-before-define": ["error", "nofunc"],
-        "object-curly-newline": ["error",
-            {
-                "consistent": true
-            }],
         "object-shorthand": ["error", "consistent-as-needed"],
-        "operator-linebreak": ["error", "after"],
         "one-var": ["error", "never"],
-        "padded-blocks": ["error", "never"],
-        "space-before-function-paren": ["error",
-            {
-                "asyncArrow": "always",
-                "anonymous": "always",
-                "named": "always"
-            }],
         "strict": ["error", "global"],
         "quotes": ["error", "double"],
         "valid-jsdoc": ["error",
