88fb54b92493992005757e6f935dc85e26bf8f5d
Alexander St Laurent <stlaurent.alexander@protonmail.com>
2019-03-30 11:53:16 -0400

    remove use of make and gem as dev tools

--- webpack.common.js
+++ webpack.common.js
@@ -7,5 +7,8 @@ module.exports = {
     "output": {
         "filename": "[name].bundle.js",
         "path": path.resolve(__dirname, "docs")
+    },
+    "devServer": {
+        "contentBase": "./docs"
     }
 };
